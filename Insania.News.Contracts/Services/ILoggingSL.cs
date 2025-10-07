7164135c3f8a394efeb0dd91993e0ce3fef49239
Альтаир фон Альфхейм <insania_officialis@outlook.com>
2025-10-07 15:29:22 +0300

    - Добавление фоновой записи логов в бд - Обновление пакетов

---
+++ Insania.News.Contracts/Services/ILoggingSL.cs
@@ -0,0 +1,16 @@
+﻿using Insania.News.Entities;
+
+namespace Insania.News.Contracts.Services;
+
+/// <summary>
+/// Интерфейс сервиса фонового логгирования в бд
+/// </summary>
+public interface ILoggingSL
+{
+    /// <summary>
+    /// Метод постановки лога в очередь на обработку
+    /// </summary>
+    /// <param cref="LogApiNews" name="log">Лог для записи</param>
+    /// <returns cref="ValueTask">Задание</returns>
+    ValueTask QueueLogAsync(LogApiNews log);
+}
